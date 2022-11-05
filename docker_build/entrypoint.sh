dbd783b07162b5e3a7a5b10c01c57659786c32bd
Dmitry Tolstykh <d.v.tolstykh@live.ru>
2022-11-05 17:56:22 +0300

    Debug #62

--- docker_build/entrypoint.sh
+++ docker_build/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+# Environment settings
 set -x
 
+# Sync jenkins user context for getting neccessary permissions
 groupmod -og ${DOCKERGID}  docker
 groupadd -og ${JENKINSGID} jenkins
 useradd -g jenkins -G docker -m -N -u ${JENKINSUID} -c "Jenkins action account" jenkins
 
+# Set script behavior as jenkins expects (endless waiting for stdin input)
 set -- gosu jenkins "$@"
-cat
+sleep 1d
