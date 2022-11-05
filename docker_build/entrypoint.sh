c367342dc6f7dfe91903c793a5141d4b8e070e87
Dmitry Tolstykh <d.v.tolstykh@live.ru>
2022-11-05 17:33:25 +0300

    Debug #61

--- docker_build/entrypoint.sh
+++ docker_build/entrypoint.sh
@@ -5,3 +5,6 @@ set -x
 groupmod -og ${DOCKERGID}  docker
 groupadd -og ${JENKINSGID} jenkins
 useradd -g jenkins -G docker -m -N -u ${JENKINSUID} -c "Jenkins action account" jenkins
+
+set -- gosu jenkins "$@"
+cat
